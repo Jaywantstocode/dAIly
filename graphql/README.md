c869271a5e09cd9a6cc1d54466012ab00970c921
Jayson <jayson.hao@mail.utoronto.ca>
2023-01-21 03:36:30 -0500

    initial commit for backend, wip

---
+++ graphql/README.md
@@ -0,0 +1,11 @@
+# Set up for Backend Environment
+
+## Requirement
+pyenv
+
+## Setup
+pyenv install 3.9.12
+pyenv local 3.9.12
+
+## installing required packages
+pip install -r requirements.txt
---
+++ graphql/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+from flask_graphql import GraphQLView
+from mongoengine import connect
+
+from schema import schema
+
+db = MongoEngine()
+app = Flask(__name__)
+
+app.config["MONGODB_SETTINGS"] = [
+    {
+        "db": "project1",
+        "host": "localhost",
+        "port": 27017,
+        "alias": "default",
+    }
+]
+db.init_app(app)
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
+    'graphql',
+    schema=schema,
+    graphiql=True,
+))
+
+
+if __name__ == '__main__':
+    app.run()
+    
+
