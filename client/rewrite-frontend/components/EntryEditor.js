6ea9e66398c7ef8ca648d20e46dea3b0c10f48e4
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-01-21 15:50:16 -0500

    make minimal editor with reactquill

--- client/rewrite-frontend/components/EntryEditor.js
+++ client/rewrite-frontend/components/EntryEditor.js
@@ -1,38 +1,18 @@
-// import { useEditor, EditorContent } from '@tiptap/react'
-// import StarterKit from '@tiptap/starter-kit'
-// import '../styles/EntryEditor.module.scss';
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
-// import dynamic from 'next/dynamic';
-// const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-// import 'react-quill/dist/quill.snow.css';
-
-// function App() {
-//     const [value, setValue] = useState('')
-//     return(
-//        <ReactQuill value={value} onChange={setValue}/>
-//     )
-// }
-
-// export default App;
+import 'react-quill/dist/quill.snow.css';
 
 /**
  * Create a markdown editor in the page. Edit mode/view modes available.
  */
 const EntryEditor = ({content}) => {
-  const editor = useEditor({
-    editorProps: {
-      attributes: {
-        class: 'test',
-      }
-    },
-    extensions: [
-      StarterKit,
-    ],
-    content: content,
-  })
+  const [value, setValue] = useState(content)
+
   return (
     <>
-      <EditorContent editor={editor} />
+       <ReactQuill value={value} onChange={setValue}/>
     </>
   );
 }
