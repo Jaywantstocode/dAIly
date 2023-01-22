25015f505865e4eec166bd1d9c41ec76185b5239
Jayson <jayson.hao@mail.utoronto.ca>
2023-01-22 01:54:48 -0500

    diaries query fixed

--- graphql/diary/query.py
+++ graphql/diary/query.py
@@ -3,6 +3,9 @@ from graphene.relay import Node
 from graphene_mongo import MongoengineObjectType, MongoengineConnectionField
 from graphql_relay import from_global_id
 from diary.model import DiaryModel
+from user.model import UserModel
+from mongoengine.queryset.visitor import Q
+
 
 class Diary(MongoengineObjectType):
     
@@ -18,8 +21,9 @@ class Query(graphene.ObjectType):
         diary = DiaryModel.objects.get(pk=from_global_id(id)[1])
         return diary
         
-    diaries = MongoengineConnectionField(Diary)
-    def resolve_diaries(root, info, id=graphene.ID(), **kwargs):
-        diaries = MongoengineConnectionField(Diary)
-        return diaries.default_resolver(root, info, user=id, **kwargs)
+    diaries = MongoengineConnectionField(Diary, start_date = graphene.Date(), end_date = graphene.Date())
+    def resolve_diaries(root, info, id=graphene.ID(), start_date = graphene.Date(), end_date = graphene.Date(), **kwargs):
+        user = UserModel.objects.get(pk=from_global_id(id)[1])
+        diaries = DiaryModel.objects(Q(user=user) & Q(date__gte=start_date) & Q(date__lte=end_date))
+        return diaries
         
