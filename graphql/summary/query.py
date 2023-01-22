573eabb8f17a4027d46724e62fb16731133386e2
Jayson <jayson.hao@mail.utoronto.ca>
2023-01-22 06:08:23 -0500

    fixed summary queries

--- graphql/summary/query.py
+++ graphql/summary/query.py
@@ -3,6 +3,7 @@ from graphene.relay import Node
 from graphene_mongo import MongoengineObjectType, MongoengineConnectionField
 from graphql_relay import from_global_id
 from summary.model import SummaryModel
+from user.model import UserModel
 
 class Summary(MongoengineObjectType):
     
@@ -14,13 +15,14 @@ class Summary(MongoengineObjectType):
     
 class Query(graphene.ObjectType):
     summary = graphene.Field(Summary, id=graphene.ID())
-    def resolve_user(root, info, id):
+    def resolve_summary(root, info, id):
         summary = SummaryModel.objects.get(pk=from_global_id(id)[1])
         # if user.is_deleted is True:
         #     return GraphQLError('This quest has already been deleted.')
         return summary
 
     summaries = MongoengineConnectionField(Summary)
-    def resolve_diaries(root, info, id=graphene.ID(), **kwargs):
+    def resolve_summaries(root, info, id=graphene.ID(), **kwargs):
         summaries = MongoengineConnectionField(Summary)
-        return summaries.default_resolver(root, info, user=id, **kwargs)
+        user = UserModel.objects.get(pk=from_global_id(id)[1])
+        return summaries.default_resolver(root, info, user=user, **kwargs)
