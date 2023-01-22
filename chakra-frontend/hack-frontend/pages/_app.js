bdbd7b93e81c1b4fb1c7f3ccf7282b616a8d9af3
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-01-22 01:01:27 -0500

    first query

--- chakra-frontend/hack-frontend/pages/_app.js
+++ chakra-frontend/hack-frontend/pages/_app.js
@@ -1,27 +1,54 @@
-import Nav from '@/components/Nav'
-import { ChakraProvider } from '@chakra-ui/react'
-import { extendTheme } from '@chakra-ui/react'
+import Nav from "@/components/Nav";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-import "../styles/globals.scss"
+import "../styles/globals.scss";
 
 const colors = {
   brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
   },
-}
+};
 
-const theme = extendTheme({ colors })
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+
+client
+  .mutate({
+    mutation: gql`
+      mutation {
+        createUser(input: { name: "jack", email: "jacky@gmail.com" }) {
+          user {
+            id
+            email
+            name
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log("RESULT -> ", result.data));
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Nav />
       <Component {...pageProps} />
-
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
