406f5cd12730cec56339d1e24d922f306ef68665
Gaelan McMillan <73502924+gaelanmcmillan@users.noreply.github.com>
2023-01-22 00:40:55 -0500

    Frontend Site (#3)
    * experiment with prompt's effect on generation
    
    * add run script, more logs
    
    * add run script, more logs
    
    * add babelrc
    
    * add pages and page transition with framer
    
    * go from yarn->npm, css->scss. add tiptap
    
    * go from tiptap->reactquill
    
    * make minimal editor with reactquill
    
    * make minimal no-library text editor, footer and nav. beginning to integrate chakra, so this is a reset point.
    
    * fail to integrate chakra
    
    * restart frontend with chakra and next13
    
    * remove old frontend from chakra branch
    
    * basic nav working
    
    * HURRAY! nice looking calendar
    
    * add svgs, remove template svgs
    
    * middle of font integration

---
+++ chakra-frontend/hack-frontend/components/Nav.js
@@ -0,0 +1,131 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import styles from '../styles/Nav.module.scss'
+import { HamburgerIcon, CloseIcon, SettingsIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
+
+const pages = [
+  { name: "Calendar", route: "/calendar" },
+  { name: "Summaries", route: "/summaries" },
+  { name: "Memories", route: "/memories" },
+];
+
+const NavLink = ({ name, route, active }) => {
+  // const bg = active ? `url(/images/bookmark.svg)` : "";
+  const sizeIncrease = active ? 150 : 0;
+  return <Link
+    className={active? styles.topactive : ""}
+    px={2}
+    py={1}
+    rounded={"md"}
+    fontWeight={active? "bold" : ""}
+    // border="1px solid black"
+    // position="fixed"
+    // bg hack
+    // pb={`${sizeIncrease}`}
+    // mb={`-${sizeIncrease}`}
+    // bg={bg}
+    // bgSize="150px"
+    // bgRepeat="no-repeat"
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={route}
+  >
+    <Box
+    // h="100px"
+    fontSize="20pt"
+    >
+      {name}
+    </Box>
+  </Link>
+};
+
+const Nav = () => {
+  const router = useRouter();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const makeLinks = () =>
+    pages.map((page, i) => {
+      return <NavLink
+        key={i} 
+        name={page.name} 
+        route={page.route}
+        active={page.route===router.asPath}/>
+    });
+
+  return (
+    <>
+      {/* <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}> */}
+      <Box px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>Logo</Box>
+            <HStack
+              // border="1px solid red"
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {makeLinks()}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              >
+                <SettingsIcon />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Preferences</MenuItem>
+                <MenuItem>Help</MenuItem>
+                <MenuDivider />
+                <MenuItem>Sign Out</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {/* Small page */}
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {makeLinks()}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+};
+
+export default Nav;
